b78314cd1a8954c66d8b397aeea58c1a1ac163ab
eleoes <leo.escaroz@gmail.com>
2023-02-27 16:06:26 -0800

    wip: building routes, error handling

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
+const path = require('path');
 
 // Initialize the App
 const app = express();
@@ -29,6 +30,21 @@ mongoose.connection
 app.get('/', (req,res) => {
     res.send('Welcome to CraveCraft API');
 });
+
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+app.use('/', require('./controllers/root'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 not found' })
+    } else {
+        res.type('txt').send('404 not found')
+    }
+})
 
 // Tell the App to Listen
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
